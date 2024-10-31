1b2a965c33302a7709911cf0a90be9887c34aed7
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2024-10-30 21:52:08 -0400

    edited filtering mechanism

--- filtering.py
+++ filtering.py
@@ -5,17 +5,20 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
 # dataframe loads in csv file
-df = pd.read_csv('ADDFILEHERE', index_col = 'COLUMNFORINDEX')
+df = pd.read_csv('generated_users.csv')
 
-# finding unique values and 
+# finding unique course codes from csv
 unique_codes  = df['course_code'].unique()
 
-# dictionary to hold all course codes
+# create empty dictionary to hold all course codes
 course_codes = {}
 
+# add each course code into course_codes dictionary
 for code in unique_codes:
     course_codes[code] = df[df['course_code'] == code]
 
-#k means clustering on each group
-for code in unique_codes:
-    course_codes['specific_code']
+print(course_codes['CS135'])
+
+# #k means clustering on each group
+# for code in unique_codes:
+#     course_codes['specific_code']

d309a096e423985542e3388ff35c138b8cdc47c7
alyssadong2006 <alydong2006@gmail.com>
2024-11-17 17:27:40 -0500

    frontend

--- backend/studbud/studbud/settings.py
+++ backend/studbud/studbud/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homepage'
+    'homepage',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

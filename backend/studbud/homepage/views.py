8db57644db44fc8640a4a7d51ff464ab3028ec17
krpchandok <kirpachandok@gmail.com>
2024-11-17 17:26:43 -0500

    more backend

--- backend/studbud/homepage/views.py
+++ backend/studbud/homepage/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from homepage.models import User
 
 class HomeView(TemplateView):
     template_name = 'studbud/homepage.html'
+
+    def UserList(request):
+        user_list = User.objects.all()
+            return render(request, 'homepage/user_list.html', {'Top Users': users})

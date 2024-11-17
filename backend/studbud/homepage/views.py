fbe0c58be05fcb8f933fbb04030743fc0c54372e
alyssadong2006 <alydong2006@gmail.com>
2024-11-17 15:21:50 -0500

    csrf

--- backend/studbud/homepage/views.py
+++ backend/studbud/homepage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class HomeView(TemplateView):
-    template_name = 'backend/homepage.html'
+    template_name = 'studbud/homepage.html'

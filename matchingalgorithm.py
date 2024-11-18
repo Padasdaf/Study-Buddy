0640a65b89db8df62c8eeba2762420841056f9fa
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2024-11-17 22:20:18 -0500

    edited algorithm

--- matchingalgorithm.py
+++ matchingalgorithm.py
@@ -1,11 +1,10 @@
-import pandas as pd
-import random
-import matplotlib.pyplot as plt
+import pandas as pd # for data processing
+import random # for testing data
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import pairwise_distances
 
-
+#################################################
 classes = ["CS135", "MATH135", "MATH137", "COMMST223", "ECON101"]
 genders = ["Male", "Female"]
 study_times = ["Morning", "Afternoon", "Night"]
@@ -13,7 +12,7 @@ personalities = ["Introvert", "Extrovert"]
 learning_styles = ["Audio", "Visual", "Kinaesthetic"]
 names = ["a", "b", "c", "d"]
 
-
+# test data generated
 data = {
     "user_id": [random.choice(names) for _ in range (100)],
     "class": [random.choice(classes) for _ in range(100)],
@@ -23,58 +22,88 @@ data = {
     "learning_style": [random.choice(learning_styles) for _ in range(100)]
 }
 
-user_data = pd.DataFrame(data)
-# %%
-unique_codes = user_data['class'].unique()
+# user_data will be read from database when finished
+def buddy_generator(data):
+    user_data = pd.DataFrame(data)
+    
+    #####################################################
+    # pre-processing: find number of courses, encode categorical data, scale data
+    unique_codes = user_data['class'].unique()
+    course_codes = {}
 
-course_codes = {}
-
-for code in unique_codes:
-    course_codes[code] = user_data[user_data['class'] == code]
+    for code in unique_codes:
+        course_codes[code] = user_data[user_data['class'] == code]
 
 
-# find the number of distinct courses in user_data -> used to create the number of clusters, k
+    # find the number of distinct courses in user_data -> used to create the number of clusters, k
 
-# encode categorial variables -> gender, personality and preferred study time
-label_encoder_gender = LabelEncoder()
-user_data['gender'] = label_encoder_gender.fit_transform(user_data['gender'])
+    # encode categorial variables -> gender, personality and preferred study time
 
-label_encoder_personality = LabelEncoder()
-user_data['personality'] = label_encoder_personality.fit_transform(user_data['personality'])
+    label_encoder_gender = LabelEncoder()
+    user_data['gender'] = label_encoder_gender.fit_transform(user_data['gender'])
 
-label_encoder_studytime = LabelEncoder()
-user_data['preferred_study_time'] = label_encoder_studytime.fit_transform(user_data['preferred_study_time'])
+    label_encoder_personality = LabelEncoder()
+    user_data['personality'] = label_encoder_personality.fit_transform(user_data['personality'])
 
-label_encoder_learning = LabelEncoder()
-user_data['learning_style'] = label_encoder_learning.fit_transform(user_data['learning_style'])
+    label_encoder_studytime = LabelEncoder()
+    user_data['preferred_study_time'] = label_encoder_studytime.fit_transform(user_data['preferred_study_time'])
 
-# print(user_data.head())
-# print (user_data['preferred_study_time'].to_string(index=False))
+    label_encoder_learning = LabelEncoder()
+    user_data['learning_style'] = label_encoder_learning.fit_transform(user_data['learning_style'])
 
-# %%
-# scale the data so all features contribute equally to the model
-scaler = StandardScaler()
+    # scale the data so all features contribute equally to the model
+    scaler = StandardScaler()
 
-features_to_scale = ['gender', 'preferred_study_time', 'personality', 'learning_style']
-user_data_scaled = user_data.copy()
+    features_to_scale = ['gender', 'preferred_study_time', 'personality', 'learning_style']
+    user_data_scaled = user_data.copy()
 
-user_data_scaled[features_to_scale] = scaler.fit_transform(user_data[features_to_scale])
+    user_data_scaled[features_to_scale] = scaler.fit_transform(user_data[features_to_scale])    
 
-#print (user_data_scaled.head())
+    unique_codes = user_data_scaled['class'].unique()
+    course_codes = {code: user_data_scaled[user_data_scaled['class'] == code] for code in unique_codes}
 
-# %%
+    #print(course_codes.items())
+
+    #############################################################
+    # using knn
+
+    final_groupings = []
+
+    for class_label, class_data in course_codes.items():
+        print(class_label, class_data)
+        num_users = len(class_data)
+    # find number of users to determine number of clusters
+        if divmod(num_users, 5) == 0:
+            num_clusters = num_users / 5
+        elif divmod(num_users, 5) == 3 or divmod(num_users, 5) == 4:
+            num_clusters = num_users // 5 + 1
+        else: 
+            num_clusters = num_users // 5
+
+        kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+        labels = kmeans.fit_predict(class_data)
+
+        clusters = [[] for _ in range(n_clusters)]
+        for point, label in zip(class_data, labels):
+            clusters[label].append(point.tolist())
+        
+        final_groupings.append(clusters)
+
+    print(final_groupings)
+
+buddy_generator(data)
+
+# issue: a, b, c, d messing up algo. need to make sure user_id not factored in - only look at some features
+
+    
+"""
 # apply k means clustering
-
-unique_codes = user_data_scaled['class'].unique()
-course_codes = {code: user_data_scaled[user_data_scaled['class'] == code] for code in unique_codes}
 
 def generate_buddies (user_info, course_codes, features):
     course = user_info['class']
 
     group = course_codes[course].copy()
 
-
-    num_clusters = max(1, len(group) // 5)
     kmeans = KMeans(n_clusters=num_clusters, random_state=42)
     group['cluster'] = kmeans.fit_predict(group[features])
 
@@ -94,4 +123,4 @@ def generate_buddies (user_info, course_codes, features):
 user_info = user_data_scaled.iloc[0] # first user in dataframe
 top_5_buddies = generate_buddies(user_info, course_codes, features_to_scale)
 print (top_5_buddies)
-
+"""
